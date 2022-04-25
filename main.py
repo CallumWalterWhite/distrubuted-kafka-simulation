814f3817b9e8b6198281f9b0066ab7864f49bc1f
callum white <callumwhi@live.co.uk>
2022-04-25 16:33:33 +0100

    add mqtt client connection thread

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import paho.mqtt.client as mqtt
+from threading import Thread
 
 app = Flask(__name__)
 
@@ -13,4 +15,31 @@ def index():
     }]
     return render_template('index.html', posts=posts)
 
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+def createClient():
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect("mqtt.eclipseprojects.io", 80, 60)
+
+    # Blocking call that processes network traffic, dispatches callbacks and
+    # handles reconnecting.
+    # Other loop*() functions are available that give a threaded interface and a
+    # manual interface.
+    client.loop_forever()
+
+t = Thread(target=createClient)
+t.start()
 app.run()
